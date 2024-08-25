dd4e45e099b3f8b4c89156299f33da14b29991a2
SriRam-Sam02 <sriram4sam@gmail.com>
2024-08-25 16:37:09 +0530

    Update typewise-alert.cpp

--- typewise-alert.cpp
+++ typewise-alert.cpp
@@ -1,13 +1,11 @@
 #include "typewise-alert.h"
 #include <stdio.h>
 
-BreachType inferBreach(double value, CoolingLimits range) 
-{
+BreachType inferBreach(double value, CoolingLimits range) {
     return (value < range.lowerLimit) ? TOO_LOW : (value > range.upperLimit) ? TOO_HIGH : NORMAL;
 }
 
-CoolingLimits getTemperatureRange(CoolingType coolingType) 
-{
+CoolingLimits getTemperatureRange(CoolingType coolingType) {
     CoolingLimits ranges[] = {
     {0, 35}, /*PASSIVE_COOLING*/
     {0, 45}, /*HI_ACTIVE_COOLING*/
@@ -16,8 +14,7 @@ CoolingLimits getTemperatureRange(CoolingType coolingType)
   return ranges[coolingType];
 }
 
-BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) 
-{
+BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC) {
   CoolingLimits range = getTemperatureRange(coolingType);
   return inferBreach(temperatureInC, range);
 }
